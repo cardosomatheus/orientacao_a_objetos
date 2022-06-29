3648248da1db39711872f25ca288f8507f912158
matheus <cardosomcds1@gmail.com>
2022-06-29 15:49:49 -0300

    commit #2

--- metodos getter e setter.py
+++ metodos getter e setter.py
@@ -1,10 +1,12 @@
 class Teste:
-    """ Conceito de encapsulamento de objetos"""
+    """ Conceito de encapsulamento de objetos
+        Método Getter = recupera valores armazenados no objeto
+        Método Setter = altera o valor da variavel"""
     def __init__(self, valor):
         self.x = valor
 
     def get_valor(self):
-        """ Método getter para return do atributo x"""
+        """ Método getter com o return do atributo x"""
         return self.x
 
     def set_valor(self, v):
@@ -13,8 +15,10 @@ class Teste:
 
 
 teste = Teste(10)
-print(f'valor de objeto:  {teste.get_valor()}')
+print(f'valor de objeto:  {teste.get_valor()}')     #leitura do valor na variavel
+print('-'*10)
+
 valor = int(input('Digite um valor numerico: '))
-teste.set_valor(valor)
+teste.set_valor(valor)  # Alteração do valor da variavel
 print(f'valor de objeto após a atribuição setter:  {teste.get_valor()}')
 
